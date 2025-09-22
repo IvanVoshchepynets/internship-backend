a4f7f8edc558b08ee10cddff5d00df82c44dcf08
IvanVoshchepynets <ivan.voshchepynets.ki.2019@lpnu.ua>
2025-09-22 10:04:00 +0300

    feat(auth): added basic registration, login with JWT

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
 import AutoLoad from "@fastify/autoload";
+import fastifyJwt from "@fastify/jwt";
 import type { FastifyPluginAsync } from "fastify";
 import path from "path";
-import fastifyJwt from "@fastify/jwt";
 
 const app: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.register(fastifyJwt, {
-    secret: process.env.JWT_SECRET || "supersecret",
-  });
+	fastify.register(fastifyJwt, {
+		secret: process.env.JWT_SECRET || "supersecret",
+	});
 
-  void fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "plugins"),
-    options: opts,
-  });
+	void fastify.register(AutoLoad, {
+		dir: path.join(__dirname, "plugins"),
+		options: opts,
+	});
 
-  void fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "modules"),
-    options: opts,
-    dirNameRoutePrefix: false,
-  });
+	void fastify.register(AutoLoad, {
+		dir: path.join(__dirname, "modules"),
+		options: opts,
+		dirNameRoutePrefix: false,
+	});
 };
 
 export default app;
