7b4ca2c5e6d59d7fb792c894d2569ecf80e607cc
IvanVoshchepynets <ivan.voshchepynets.ki.2019@lpnu.ua>
2025-09-23 15:19:04 +0300

    fix(auth): added name, new fail, validationn

--- src/modules/auth/schemas.ts
+++ src/modules/auth/schemas.ts
@@ -1,22 +1,22 @@
 export const registerSchema = {
-  body: {
-    type: "object",
-    required: ["name", "email", "password"],
-    properties: {
-      name: { type: "string", minLength: 2 },
-      email: { type: "string", format: "email" },
-      password: { type: "string", minLength: 6 },
-    },
-  },
+	body: {
+		type: "object",
+		required: ["name", "email", "password"],
+		properties: {
+			name: { type: "string", minLength: 2 },
+			email: { type: "string", format: "email" },
+			password: { type: "string", minLength: 6 },
+		},
+	},
 } as const;
 
 export const loginSchema = {
-  body: {
-    type: "object",
-    required: ["email", "password"],
-    properties: {
-      email: { type: "string", format: "email" },
-      password: { type: "string", minLength: 6 },
-    },
-  },
+	body: {
+		type: "object",
+		required: ["email", "password"],
+		properties: {
+			email: { type: "string", format: "email" },
+			password: { type: "string", minLength: 6 },
+		},
+	},
 } as const;
