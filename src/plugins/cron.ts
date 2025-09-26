5d9b55cfcb89088446ae2aa8f196240416d3c265
IvanVoshchepynets <ivan.voshchepynets.ki.2019@lpnu.ua>
2025-09-26 21:08:19 +0300

    feat(cron): add cron plugin

--- src/plugins/cron.ts
+++ src/plugins/cron.ts
@@ -3,15 +3,15 @@ import cron from "node-cron";
 import { parseFeed } from "../services/feedParser";
 
 export default fp(async (fastify) => {
-  fastify.log.info("Cron plugin registered");
+	fastify.log.info("Cron plugin registered");
 
-  cron.schedule("*/5 * * * *", async () => {
-    try {
-      const DEFAULT_FEED_URL = "https://www.pravda.com.ua/rss/view_news";
-      await parseFeed(fastify, DEFAULT_FEED_URL, 1); 
-      fastify.log.info("Feed updated by cron");
-    } catch (err) {
-      fastify.log.error("Cron feed update failed", err);
-    }
-  });
+	cron.schedule("*/5 * * * *", async () => {
+		try {
+			const DEFAULT_FEED_URL = "https://www.pravda.com.ua/rss/view_news";
+			await parseFeed(fastify, DEFAULT_FEED_URL, 1);
+			fastify.log.info("Feed updated by cron");
+		} catch (err) {
+			fastify.log.error("Cron feed update failed", err);
+		}
+	});
 });
