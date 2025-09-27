83cc5005fea544b2cab2c1b377dad2355266db93
IvanVoshchepynets <ivan.voshchepynets.ki.2019@lpnu.ua>
2025-09-27 18:50:48 +0300

    fix(plugin): change pluginLoaded

--- src/plugins/sensible.plugin.ts
+++ src/plugins/sensible.plugin.ts
@@ -4,11 +4,11 @@ import fp from "fastify-plugin";
 const pluginName = "sensible-plugin";
 
 export default fp(
-  async (fastify) => {
-    await fastify.register(sensible);
-    fastify.log.info(`${pluginName} loaded`);
-  },
-  {
-    name: pluginName,
-  },
+	async (fastify) => {
+		await fastify.register(sensible);
+		fastify.log.info(`${pluginName} loaded`);
+	},
+	{
+		name: pluginName,
+	},
 );
