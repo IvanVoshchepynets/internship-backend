10e913f253dafb0739498d5726d3f1050b53eb3f
IvanVoshchepynets <ivan.voshchepynets.ki.2019@lpnu.ua>
2025-09-28 20:53:24 +0300

    feat: voshchepynetsauction and swagger ui

--- src/plugins/swagger.plugin.ts
+++ src/plugins/swagger.plugin.ts
@@ -1,27 +1,27 @@
-import fp from "fastify-plugin";
 import swagger from "@fastify/swagger";
 import swaggerUi from "@fastify/swagger-ui";
+import fp from "fastify-plugin";
 
 export default fp(async (fastify) => {
-  await fastify.register(swagger, {
-    openapi: {
-      info: {
-        title: "Feed API",
-        description: "Documentation API for internship-backend",
-        version: "1.0.0",
-      },
-    },
-  });
+	await fastify.register(swagger, {
+		openapi: {
+			info: {
+				title: "Feed API",
+				description: "Documentation API for internship-backend",
+				version: "1.0.0",
+			},
+		},
+	});
 
-  await fastify.register(swaggerUi, {
-    routePrefix: "/docs",
-    staticCSP: true,
-    transformStaticCSP: (header) => header,
-    uiConfig: {
-      docExpansion: "list",
-      deepLinking: false,
-    },
-  });
+	await fastify.register(swaggerUi, {
+		routePrefix: "/docs",
+		staticCSP: true,
+		transformStaticCSP: (header) => header,
+		uiConfig: {
+			docExpansion: "list",
+			deepLinking: false,
+		},
+	});
 
-  fastify.log.info("Swagger UI available за /docs");
+	fastify.log.info("Swagger UI available за /docs");
 });
